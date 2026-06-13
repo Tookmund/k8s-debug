a5de7e6b0650607f6a42bdd6a48210d7da6b3f2b
Jacob Adams <jacob@tookmund.com>
2026-06-13 15:00:06 -0400

    entrypoint.sh: Don't source the ssh defaults file if we don't use it

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ echo "$AUTHORIZED_KEYS" > /root/.ssh/authorized_keys
 chmod 700 /root/.ssh
 chmod 600 /root/.ssh/authorized_keys
 
-. /etc/default/ssh || true
-
 /usr/sbin/sshd -t
 
 exec /usr/sbin/sshd
